7cbc515ccf5a5bcbb6882d348764cdb3b5135f97
Andrey <58521853+sw3zeg@users.noreply.github.com>
2025-04-07 06:00:24 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
+export const options = {
+  stages: [
+    { duration: '5s', target: 5 },
+    { duration: '10s', target: 5 },
+    { duration: '5s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('http://host.docker.internal:8080/api/books');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+}
