2c10f0b62f5ecbca56374fd37a3530c0274b1363
Andrey <58521853+sw3zeg@users.noreply.github.com>
2025-05-22 00:45:05 +0300

    Update script.js

--- k6/script.js
+++ k6/script.js
@@ -11,7 +11,7 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('http://172.20.10.3:8081/api/books');
+  const res = http.get('http://localhost:8081/api/books');
 
   check(res, {
     'status is 200': (r) => r.status === 200,
